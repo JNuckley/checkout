96b89da810bb55593ef3496f5a3043d392be241c
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-03 11:57:33 +0000

    Checkout can find a product by name

--- checkout.rb
+++ checkout.rb
@@ -1,3 +1,23 @@
 class Checkout
+attr_accessor :items
+
+def initialize
+  @items = [
+    {product:'Banana', cost: '£0.75'},
+    {product: 'Bread', cost: '£1.10'}
+  ]
+end
+
+def find_item(product)
+  @items.find{|item| item[:product] == product }
+end
 
 end
+
+# As a shopper
+# So I know how much an item costs
+# I would like to be able to see its price
+
+# As a shopper
+# So that I can buy an item
+# I would like to be able to scan items at the checkout
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -4,4 +4,10 @@ describe Checkout do
   it 'creates an instance of Checkout' do
     checkout = Checkout.new
   end
+  it 'can find items' do
+    checkout = Checkout.new
+    item = checkout.find_item('Banana')
+    expect(item).to eq(product:'Banana', cost: '£0.75')
+
+  end
 end
