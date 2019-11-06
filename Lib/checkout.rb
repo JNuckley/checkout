38cc9fa228b8a8ec79eb1168114435280b77f30c
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-06 11:28:45 +0000

    added a test for the scan method

--- Lib/checkout.rb
+++ Lib/checkout.rb
@@ -1,18 +1,22 @@
 class Checkout
 attr_accessor :items
 
+#  have the items available for reference
 def initialize
   @items = [
     {product:'Banana', cost: '£0.75'},
     {product: 'Bread', cost: '£1.10'}
   ]
 end
-
+#  find an item by product name
 def find_item(product)
   @items.find{|item| item[:product] == product }
 end
-
 end
+
+# def scan(price)
+#   @items.find{|item| item[:cost] == cost}
+# end
 
 # As a shopper
 # So I know how much an item costs
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,13 +1,16 @@
-require './checkout'
+require './Lib/checkout'
 
 describe Checkout do
-  it 'creates an instance of Checkout' do
-    checkout = Checkout.new
-  end
+  subject(:checkout) {Checkout.new}
+
+  it { is_expected.to be_an_instance_of(Checkout) }
+
   it 'can find items' do
-    checkout = Checkout.new
     item = checkout.find_item('Banana')
     expect(item).to eq(product:'Banana', cost: '£0.75')
-
+  end
+  it 'can scan an item and return the price' do
+    item = checkout.scan('Banana')
+    expect(item).to eq(cost: '£0.75')
   end
 end
