e3400133f4c81be168647fb44dc163f1a7bd83b7
Jo Nuckley <jnuckley@yahoo.co.uk>
2019-11-08 12:21:03 +0000

    pushing cost of scanned items to bill

--- Lib/checkout.rb
+++ Lib/checkout.rb
@@ -4,10 +4,12 @@ class Checkout
   #  have the items available for reference
   def initialize
     @items = [
-      {product:'Banana', cost: '£0.75'},
-      {product: 'Bread', cost: '£1.10'}
+      {product: 'Banana', cost: '0.75'},
+      {product: 'Bread', cost: '1.10'}
     ]
     @scanned = []
+    @bill = []
+
   end
   #  find an item by product name
   def find_item(product)
@@ -15,14 +17,11 @@ class Checkout
   end
 
   def scan(product)
-  @scanned << @items.find{|item| item[:product] == product}
+    @scanned << @items.find{|item| item[:product] == product}
+
+  end
+
+  def bill
+    @bill = (@scanned.map {|cost| cost[:cost]})
   end
 end
-
-# As a shopper
-# So I know how much an item costs
-# I would like to be able to see its price
-
-# As a shopper
-# So that I can buy an item
-# I would like to be able to scan items at the checkout
--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -7,11 +7,16 @@ describe Checkout do
 
   it 'can find items' do
     item = checkout.find_item('Banana')
-    expect(item).to eq(product:'Banana', cost: '£0.75')
+    expect(item).to eq(product:'Banana', cost: '0.75')
   end
 
   it 'can scan an item and return the price' do
     item = checkout.scan('Banana')
-    expect(item).to eq([product: 'Banana', cost: '£0.75'])
+    expect(item).to eq([product: 'Banana', cost: '0.75'])
+  end
+
+  it 'can take the price from the scanned array and add to bill' do
+    item = checkout.scan('Banana')
+    expect(checkout.bill).to eq(['0.75'])
   end
 end
